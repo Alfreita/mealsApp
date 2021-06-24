5aa4ccdb351118896af672e87f43ae953042d34a
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-06-24 15:22:58 -0300

    adding flatlist with dummy itens

--- screens/CategoriesScreens.tsx
+++ screens/CategoriesScreens.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData: any) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const Categories = (props: any) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screens</Text>
-      <Button
-        title="go to meals"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-    </View>
+    <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
   );
 };
 const styles = StyleSheet.create({
@@ -19,6 +20,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
 export default Categories;
