4c3333b1360448b1b4b3813e4aace9152d78939b
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-06-29 14:44:33 -0300

    navigate to meal detail

--- screens/CategoryMealsScreens.tsx
+++ screens/CategoryMealsScreens.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
-const CategoryMeals = (props: any) => {
+const CategoryMeals = (props: any) => {  
   const catId = props.navigation.getParam("categoryId");
 
   const displayedMeals = MEALS.filter(
@@ -13,7 +13,14 @@ const CategoryMeals = (props: any) => {
       <View style={styles.MealItem}>
         <MealItem
           title={itemData.item.title}
-          onSelectMeal={() => {}}
+          onSelectMeal={() => {
+            props.navigation.navigate({
+              routeName: "MealDetail",
+              params: {
+                mealId: itemData.item.id,
+              },
+            });
+          }}
           duration={itemData.item.duration}
           complexity={itemData.item.complexity}
           affordability={itemData.item.affordability}
--- screens/MealsDetailScrees.tsx
+++ screens/MealsDetailScrees.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 const MealsDetail = (props: any) => {
+  const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal: any) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The Meals Detail Screens</Text>
+      <Text>{selectedMeal?.title}</Text>
     </View>
   );
+};
+MealsDetail.navigationOptions = (navigationData: any) => {
+  const mealId = navigationData.navigation.getParam("categoryId");
+  const selectedMeal = MEALS.find((meal: any) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal?.title,
+  };
 };
 const styles = StyleSheet.create({
   screen: {
