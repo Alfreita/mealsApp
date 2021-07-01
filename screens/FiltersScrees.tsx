08199c0da51870e64a1243e63be85f14ff18fa47
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-07-01 11:46:17 -0300

    adding switch in filter screen

--- screens/FiltersScrees.tsx
+++ screens/FiltersScrees.tsx
@@ -1,12 +1,52 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 import CustomHeaderButtons from "../components/HeaderButtons";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = (props: any) => {
+  return (
+    <View style={styles.container}>
+      <Text>{props.label}</Text>
+      <Switch
+        value={props.state}
+        onValueChange={props.onChange}
+        trackColor={{ false: "#ccc", true: Colors.accentColor }}
+        thumbColor={Platform.OS === "android" ? Colors.accentColor : ""}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen = (props: any) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screens</Text>
+      <Text style={styles.title}>Available Filter / Restrictions</Text>
+
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(newValue: boolean) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(newValue: boolean) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(newValue: boolean) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(newValue: boolean) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -29,8 +69,20 @@ FiltersScreen.navigationOptions = (navData: any) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+
     alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
   },
 });
 export default FiltersScreen;
